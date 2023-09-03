ea3e29b995b15fe16093e6c170a5d0d19a1cb66e
Md. Shahariar Alom <85412615+ShahriarAlom@users.noreply.github.com>
2023-09-03 20:47:30 +0600

    Update drive_upload_script.py

--- drive_upload_script.py
+++ drive_upload_script.py
@@ -5,9 +5,33 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from google.auth.transport.requests import Request
 
+
 def authenticate_with_google():
-    # ... [rest of the code remains unchanged]
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('client_secret_250384027023-pta4o2d8qrt8q8ueiujomn3drb5d664v.apps.googleusercontent.com.json', ['https://www.googleapis.com/auth/drive.file'])
+            creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+    return creds
+
+def upload_to_drive(file_path):
+    creds = authenticate_with_google()
+    service = build('drive', 'v3', credentials=creds)
+    
+    file_metadata = {'name': os.path.basename(file_path)}
+    media = MediaFileUpload(file_path, resumable=True)
+    
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(f'File ID: {file.get("id")}')
 
 if __name__ == '__main__':
     file_path = "C:/Users/USER OS/OneDrive/Desktop/drive_upload_project/ChatGPT_Conversation_v2.pdf"
+
     upload_to_drive(file_path)
